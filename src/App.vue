c6fbcb7c8e941e4ea53e8a58a53b4655235298a0
Abdul Wahab <abdulwahabsoftware1@gmail.com>
2024-01-04 18:14:46 +0500

    Added: add navigation component for header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
-  <router-view />
+  <div class="app flex">
+    <Navigation />
+    <div class="app-content flex flex-column">
+      <router-view/>
+    </div>
+  </div>
 </template>
+
+<script>
+import Navigation from './components/Navigation.vue';
+
+export default {
+components: {
+  Navigation
+}
+}
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
