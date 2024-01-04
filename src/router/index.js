7480ac9f559d43249c1c9eea784d2145c2a12397
Abdul Wahab <abdulwahabsoftware1@gmail.com>
2024-01-04 15:07:31 +0500

    Updated: update files

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'home',
+    component: HomeView
   },
-];
+]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
+})
 
-export default router;
+export default router
